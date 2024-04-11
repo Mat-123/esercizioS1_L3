061d58fee8be3861bc7bb78e84943a424cb620cd
Mat-123 <156803363+Mat-123@users.noreply.github.com>
2024-04-11 17:09:10 +0200

    aggiunta funzione delete

--- index.php
+++ index.php
@@ -17,6 +17,13 @@ $pdo = new PDO($dsn, $user, $pass, $options);
 
 $stmt = $pdo->query('SELECT * FROM users_data');
 
+if (isset($_GET['delete'])) {
+    $id = $_GET['delete'];
+    $stmt_delete = $pdo->prepare("DELETE FROM users_data WHERE id = ?");
+    $stmt_delete->execute([$id]);
+    header("Location: index.php");
+};
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -35,6 +42,8 @@ $stmt = $pdo->query('SELECT * FROM users_data');
                     <th>ID</th>
                     <th>Username</th>
                     <th>E-Mail</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
                 </tr>
             </thead>
             <tbody>
@@ -46,6 +55,7 @@ $stmt = $pdo->query('SELECT * FROM users_data');
         echo "<td>$row[username]</td>";
         echo "<td>$row[email]</td>";
         echo "<td><a href='edit.php?id=".$row["id"]."' class='btn btn-primary'>Edit</a></td>";
+        echo "<td><a href='?delete=".$row["id"]."' class='btn btn-danger' onclick='return confirm(\"Are you sure you want to delete user with ID: ".$row["id"]."?\")'>Delete</a></td>";
         echo '</tr>';
     }?>
     
