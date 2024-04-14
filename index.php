4dc9838a0a7c38598f8ff54fcd6e16de2a39c770
Mat-123 <156803363+Mat-123@users.noreply.github.com>
2024-04-14 22:16:16 +0200

    search bar integrata

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
 
 include __DIR__ . '/includes/db.php';
 
-$stmt = $pdo->query('SELECT * FROM users_data');
+$stmt = $pdo->prepare("SELECT * FROM users_data WHERE username LIKE ?");
+$stmt->execute([
+    "%$search%"
+]);
 
 if (isset($_GET['delete'])) {
     $id = $_GET['delete'];
@@ -12,6 +15,15 @@ if (isset($_GET['delete'])) {
 };
 
 include __DIR__ . '/includes/html.php'; ?>
+
+<form class="row g-3 my-3">
+    <div class="col">
+        <input type="text" name="search" class="form-control" placeholder="Cerca un utente">
+    </div>
+    <div class="col-auto">
+        <button type="submit" class="btn btn-primary mb-3">Cerca</button>
+    </div>
+</form>
 
 <table class="table">
     <thead>
